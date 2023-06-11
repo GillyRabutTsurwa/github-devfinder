993ddbe1cba541fb33b9e4041622c292ccba5121
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-06-11 19:27:55 -0400

    Fixed TypeScript Compiling Error on Error Handling

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -3,6 +3,7 @@ import { Profile } from "./interfaces/profile";
 const DOM = (function () {
     const elements = {
         input: document.querySelector(".search-input") as HTMLInputElement,
+        para: document.querySelector(".test") as HTMLParagraphElement,
     };
     return elements;
 })();
@@ -11,14 +12,14 @@ async function fetchData<T>(name: string): Promise<T> {
     try {
         const response = await fetch(`https://api.github.com/users/${name}`);
         if (!response.ok) {
-            throw new Error(response.statusText);
+            throw new Error("Trouble Finding Resuluts");
         }
-        // const data = response.json() as T;
-        const data = (await response.json()) as T;
+        // const data = response.json() as Promise<T>; // je peux utiliser ceci dont la valeur est une promesse avec notre data
+        const data = (await response.json()) as T; // ou celui ci qui contient notre data diréctement
         console.log(data);
         return data;
     } catch (error) {
-        throw new Error("error");
+        throw error;
     }
 }
 
@@ -28,12 +29,13 @@ DOM.input.addEventListener("keyup", async function (e: KeyboardEvent) {
         // and fixed e.target solution thanks to this: https://stackoverflow.com/questions/44321326/property-value-does-not-exist-on-type-eventtarget-in-typescript
         const inputValue = (e.target as HTMLInputElement).value;
         console.log(inputValue);
-        await fetchData<Profile>(inputValue);
+        let x = await fetchData<Profile>(inputValue);
+        DOM.para.textContent = x.bio;
     }
 });
 
 async function init() {
-    await fetchData<Profile>("GillyRabutTsurwa");
+    await fetchData<Profile>("GillyRabutTsurwa").then((value) => console.log(value));
 }
 
 window.addEventListener("DOMContentLoaded", init);
